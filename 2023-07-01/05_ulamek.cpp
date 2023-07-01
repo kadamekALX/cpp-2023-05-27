8f7c92a4fda3e4d07b43c71b34ace24b6b4dbb51
Krzysztof Adamek <kadamek@alx.pl>
2023-07-01 12:14:43 +0200

    mnoz - tresc

--- 2023-07-01/05_ulamek.cpp
+++ 2023-07-01/05_ulamek.cpp
@@ -3,7 +3,9 @@
 //  wypisz(u) - wypisuje Ulamek u na ekran w ładnej formie: licznik / mianownik
 //  przyjmij(u) - przyjmuje licznik i mianownik od uzytkownika i wpisuje je do Ulamka u. Nie pozwol na wprowadzenie mianownika == 0
 //  ustaw(u, a, b) - ustawia w Ulamku `u` licznik na `a` i mianownik na `b`
+//  mnoz(u, v) - domnaża Ulamek `v` do Ulamka `u`
 #include <iostream>
+
 
 struct Ulamek {
     int licznik;
@@ -33,6 +35,11 @@ int main() {
     Ulamek u;
     ustaw(&u, 1, 2);
     wypisz(u);
-    przyjmij(u); //lub przyjmij(&u)
-    wypisz(u);
+//     przyjmij(u); //lub przyjmij(&u)
+//     wypisz(u);
+    Ulamek v;
+    ustaw(&v, 2, 3);
+    wypisz(v);
+    mnoz(u, v); // mnozy `u` * `v` i wynik zapisuje w `u`
+    wypisz(u); // 1/3 lub 2/6
 }
