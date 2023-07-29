d7ad7a3410ee59a042427e903da0b1aece10a685
Krzysztof Adamek <kadamek@alx.pl>
2023-07-29 12:18:13 +0200

    Tablica szablonowa

--- 2023-07-29/05_tablica.cpp
+++ 2023-07-29/05_tablica.cpp
@@ -1,68 +1,75 @@
 #include <iostream>
+#include <string>
 
 //1. Przenieś definicje metod klasy `Tablica` na zewnątrz tej klasy
 //2. Zrób z klasy `Tablica` klasę szablonową parametryzowaną typem przechowywanych wartości
 
+template <typename T>
 class Tablica {
 public:
-    Tablica(int rozmiar);
+    Tablica(unsigned rozmiar);
     Tablica(const Tablica& kopia_tablicy);
     ~Tablica();
-    int& operator[](int indeks);
+    T& operator[](unsigned indeks);
     void wyswietl() const;
 
 private:
-    int* tab;
-    int rozmiar;
+    T* tab;
+    unsigned rozmiar;
 };
 
 
-
-Tablica::Tablica(int rozmiar) {
+template <typename T>
+Tablica<T>::Tablica(unsigned rozmiar) {
     this->rozmiar = rozmiar;
-    tab = new int[rozmiar];
+    tab = new T[rozmiar];
 }
 
-Tablica::~Tablica() {
+template <typename T>
+Tablica<T>::~Tablica() {
     delete[] tab;
 }
 
-Tablica::Tablica(const Tablica& kopia_tablicy) : rozmiar(kopia_tablicy.rozmiar){
-    tab = new int[rozmiar];
-    for (int i = 0; i < rozmiar; i ++) {
+template <typename T>
+Tablica<T>::Tablica(const Tablica& kopia_tablicy) : rozmiar(kopia_tablicy.rozmiar){
+    tab = new T[rozmiar];
+    for (unsigned i = 0; i < rozmiar; i ++) {
         tab[i] = kopia_tablicy.tab[i];
     } 
 }
 
-int& Tablica::operator[](int indeks) {
+template <typename T>
+T& Tablica<T>::operator[](unsigned indeks) {
     if (indeks >= rozmiar) {
         throw std::out_of_range("przekroczono indeks");
     }
     return tab[indeks];
 }
 
-void Tablica::wyswietl() const {
-    for (int i = 0; i < rozmiar; i++) {
+template <typename T>
+void Tablica<T>::wyswietl() const {
+    for (unsigned i = 0; i < rozmiar; i++) {
         std::cout<< i << " : " << tab[i] << '\n';
     }
 }
 
 
 int main() {
-    Tablica tab{5};
+    Tablica<int> tab{5};
     for (int i = 0; i < 5; i += 1) {
         tab[i] = i * 10;
     }
     tab.wyswietl();
-//     Tablica<int> tab{5};
-//     for (int i = 0; i < 5; i += 1) {
-//         tab[i] = i * 10;
-//     }
-//     tab.wyswietl();
-//     
-//     Tablica<double> tab2{5};
-//     for (int i = 0; i < 5; i += 1) {
-//         tab2[i] = i * 3.14;
-//     }
-//     tab2.wyswietl();
+    
+    Tablica<double> tab2{5};
+    for (int i = 0; i < 5; i += 1) {
+        tab2[i] = i * 3.14;
+    }
+    tab2.wyswietl();
+    
+    Tablica<std::string> tab3{3};
+    tab3[0] = "Ala";
+    tab3[1] = "ma";
+    tab3[2] = "kota";
+    tab3.wyswietl();
 }
