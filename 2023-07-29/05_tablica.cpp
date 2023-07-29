990abc5c117649eaf42227a034a8a49db9c4a433
Krzysztof Adamek <kadamek@alx.pl>
2023-07-29 11:55:59 +0200

    Tablica - metody osobno

--- 2023-07-29/05_tablica.cpp
+++ 2023-07-29/05_tablica.cpp
@@ -5,50 +5,64 @@
 
 class Tablica {
 public:
-    Tablica(int rozmiar) {
-        this->rozmiar = rozmiar;
-        tab = new int[rozmiar];
-    }
-    
-    ~Tablica() {
-        delete[] tab;
-    }
-    
-    Tablica(const Tablica& kopia_tablicy) : rozmiar(kopia_tablicy.rozmiar){
-        tab = new int[rozmiar];
-        for (int i = 0; i < rozmiar; i ++) {
-            tab[i] = kopia_tablicy.tab[i];
-        } 
-    }
-    
-    int& operator[](int indeks) {
-        if (indeks >= rozmiar) {
-            throw std::out_of_range("przekroczono indeks");
-        }
-        return tab[indeks];
-    }
+    Tablica(int rozmiar);
+    Tablica(const Tablica& kopia_tablicy);
+    ~Tablica();
+    int& operator[](int indeks);
+    void wyswietl() const;
 
-    void wyswietl() const {
-        for (int i = 0; i < rozmiar; i++) {
-            std::cout<< i << " : " << tab[i] << '\n';
-        }
-    }
-    
 private:
     int* tab;
     int rozmiar;
 };
 
+
+
+Tablica::Tablica(int rozmiar) {
+    this->rozmiar = rozmiar;
+    tab = new int[rozmiar];
+}
+
+Tablica::~Tablica() {
+    delete[] tab;
+}
+
+Tablica::Tablica(const Tablica& kopia_tablicy) : rozmiar(kopia_tablicy.rozmiar){
+    tab = new int[rozmiar];
+    for (int i = 0; i < rozmiar; i ++) {
+        tab[i] = kopia_tablicy.tab[i];
+    } 
+}
+
+int& Tablica::operator[](int indeks) {
+    if (indeks >= rozmiar) {
+        throw std::out_of_range("przekroczono indeks");
+    }
+    return tab[indeks];
+}
+
+void Tablica::wyswietl() const {
+    for (int i = 0; i < rozmiar; i++) {
+        std::cout<< i << " : " << tab[i] << '\n';
+    }
+}
+
+
 int main() {
-    Tablica<int> tab{5};
+    Tablica tab{5};
     for (int i = 0; i < 5; i += 1) {
         tab[i] = i * 10;
     }
     tab.wyswietl();
-    
-    Tablica<double> tab2{5};
-    for (int i = 0; i < 5; i += 1) {
-        tab2[i] = i * 3.14;
-    }
-    tab2.wyswietl();
+//     Tablica<int> tab{5};
+//     for (int i = 0; i < 5; i += 1) {
+//         tab[i] = i * 10;
+//     }
+//     tab.wyswietl();
+//     
+//     Tablica<double> tab2{5};
+//     for (int i = 0; i < 5; i += 1) {
+//         tab2[i] = i * 3.14;
+//     }
+//     tab2.wyswietl();
 }
