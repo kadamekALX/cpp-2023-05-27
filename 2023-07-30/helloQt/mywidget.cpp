6ae319a34084b3f44cc6df30c5b3e4ec6445242a
Krzysztof Adamek <kadamek@alx.pl>
2023-07-30 16:58:08 +0200

    tablica wskaznikow na przyciski

--- 2023-07-30/helloQt/mywidget.cpp
+++ 2023-07-30/helloQt/mywidget.cpp
@@ -7,10 +7,9 @@
 MyWidget::MyWidget(QWidget *parent)
     : QWidget(parent)
 {
-    //korzystając z QBoxLayout, stwórz dynamiczną
-    //siatkę 5x5 przycisków CounterButton
-    //Dodatkowo wyświetl przycisk, którego naciśnięcie
-    //zresetuje wszystkie liczniki
+    CounterButton* tab[5][5];
+//    std::vector<std::vector<CounterButton*>> tab;
+
     QPushButton* reset = new QPushButton{"RESET", this};
 
     QHBoxLayout* glowny_layout = new QHBoxLayout{this};
@@ -19,12 +18,15 @@ MyWidget::MyWidget(QWidget *parent)
         glowny_layout->addLayout(pionowy);
         for (int j = 0; j < 5; j += 1) {
 
-            CounterButton* btn = new CounterButton{this};
-            connect(reset, &QPushButton::clicked, btn, &CounterButton::resetCounter);
-            pionowy->addWidget(btn);
+            tab[i][j] = new CounterButton{this};
+            connect(reset, &QPushButton::clicked, tab[i][j], &CounterButton::resetCounter);
+            pionowy->addWidget(tab[i][j]);
         }
     }
     glowny_layout->addWidget(reset);
+
+    connect(tab[4][4], &QPushButton::clicked, tab[0][0], &CounterButton::buttonClicked);
+
 }
 
 MyWidget::~MyWidget()
